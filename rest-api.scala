c974b90dd8cdb9d84b646e5885bead51561bc508
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-10 22:43:59 +0530

    Adding Graceful Shutdown

--- rest-api.scala
+++ rest-api.scala
@@ -4,12 +4,13 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
-import scala.io.StdIn
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.{Failure, Success}
 
 object SimpleRestApi extends App {
-  implicit val system = ActorSystem("simple-rest-api")
-  implicit val materializer = ActorMaterializer()
-  implicit val executionContext = system.dispatcher
+  implicit val system: ActorSystem = ActorSystem("simple-rest-api")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   val route =
     pathEnd {
@@ -18,12 +19,20 @@ object SimpleRestApi extends App {
       }
     }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
 
-  println(s"Server online at port 8080")
-  StdIn.readLine()
+  bindingFuture.onComplete {
+    case Success(binding) =>
+      println(s"Server online at http://${binding.localAddress.getHostString}:${binding.localAddress.getPort}")
+    case Failure(ex) =>
+      println(s"Failed to bind to localhost:8080: ${ex.getMessage}")
+      system.terminate()
+  }
 
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
+  // Graceful shutdown
+  scala.sys.addShutdownHook {
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
 }
